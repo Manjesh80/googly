4bdbbd09c5fc66fd79e335bd67e2c6d94f1c8660
manjesh80 <manjesh.2j@gmail.com>
2017-12-18 07:58:21 -0500

    Added bitwise work

---
+++ bitwise/num_of_ones.py
@@ -0,0 +1,12 @@
+# page 23
+x = 3
+no_of_bits_set_to_1 = {}
+while x != 0:
+    y = x & (~(x - 1))
+    print(y)
+    no_of_bits_set_to_1[y] = y
+    x = x - y
+
+print(len(no_of_bits_set_to_1))
+
+# TODO Do for negative numbers which is two's complement
