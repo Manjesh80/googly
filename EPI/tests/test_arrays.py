ffe036b6053f51efdd9edd30645e5baf62717a2f
Gowda <magowda@corp.siriusxm.com>
2017-12-26 20:21:51 -0500

    Added multiplication test

--- EPI/tests/test_arrays.py
+++ EPI/tests/test_arrays.py
@@ -7,3 +7,12 @@ def test_separate_even_odd():
     assert arr[0] % 2 == 0
     assert arr[1] % 2 == 0
     assert arr[2] % 2 == 0
+
+
+def test_multiplication():
+    arr = multiply_two_arrays([1, 2], [1, 3])
+    print(arr)
+    assert arr[0] == 0
+    assert arr[1] == 1
+    assert arr[2] == 5
+    assert arr[3] == 6
